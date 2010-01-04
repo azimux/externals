4da0088e44d2c409028274a3aaa678d81d1780ad
Miles Georgi <azimux@gmail.com>
2010-01-03 18:41:52 -0800

    Added test for project name extraction from git repository paths

---
+++ test/test_git_project_extract_name.rb
@@ -0,0 +1,18 @@
+$:.unshift File.join(File.dirname(__FILE__), '..', 'lib') if $0 == __FILE__
+require 'externals/test_case'
+require 'externals/ext'
+require 'stringio'
+
+module Externals
+  class TestGitProjectExtractName < TestCase
+    include ExtTestCase
+
+    def test_extract_name
+      project = Externals::GitProject.new({})
+      assert_equal "test", project.extract_name("git://domain.com/test.git")
+      assert_equal "test", project.extract_name("git@domain.com:test.git")
+      assert_equal "test", project.extract_name("test.git")
+      assert_equal "test", project.extract_name("test")
+    end
+  end
+end
