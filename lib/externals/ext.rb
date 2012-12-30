4084254b3474d1df7bc6137c6b82cd00cb197690
Miles Georgi <azimux@gmail.com>
2012-12-29 22:45:17 -0800

    Bump Externals::VERSION to 1.1.1

--- lib/externals/ext.rb
+++ lib/externals/ext.rb
@@ -9,7 +9,7 @@ Dir.entries(File.join(File.dirname(__FILE__), 'extensions')).each do |extension|
 end
 
 module Externals
-  VERSION = '1.1.0'
+  VERSION = '1.1.1'
   PROJECT_TYPES_DIRECTORY = File.join(File.dirname(__FILE__), '..', 'externals','project_types')
 
   # Full commands operate on the main project as well as the externals
