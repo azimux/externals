caa9fb839f611434c0b489d82c67ebc59babaa76
Miles Georgi <azimux@gmail.com>
2026-07-23 12:06:52 -0700

    Release 2.0.0!

--- version.rb
+++ version.rb
@@ -1,5 +1,4 @@
 module Externals
   VERSION = "2.0.0".freeze
-  MINIMUM_RUBY_VERSION = [">= 2.7.0", "< 4.1"].freeze
-  # MINIMUM_RUBY_VERSION = [">= 4.0.0".freeze, "< 4.1".freeze].freeze
+  MINIMUM_RUBY_VERSION = [">= 2.7.0", "< 3.4"].freeze
 end
